efcfa9edef869a34af24f6974bb1ac32391b9d08
jilmerroca <jilmer.roca@gmail.com>
2024-05-28 00:35:54 -0500

    Infrastructure layer name fixed

--- infraestructure/src/main/java/com/codigo/clinica/mspaciente/infraestructure/dao/EmergencyContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codigo.clinica.mspaciente.infraestructure.dao;
-
-import com.codigo.clinica.mspaciente.infraestructure.entity.EmergencyContact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Long> {
-}
